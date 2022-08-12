135ad979d6d3af5a3a8ad6117120f2d07f7e6ad0
kiyoesjh <hyeyaa125@gmail.com>
2022-08-12 15:48:33 +0900

    Add: 공통 컴포넌트 추가

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+// import './style.ts';
+
+interface HeaderProps {
+  children: React.ReactNode;
+  leftButton?: React.ReactNode;
+  rightButton?: React.ReactNode;
+  style?: React.CSSProperties;
+}
+
+const Header = ({ children, leftButton, rightButton, style }: HeaderProps) => {
+  return (
+    <Wrap style={style} isButton={!!leftButton || !!rightButton}>
+      {leftButton}
+      <ChildrenWrap isButton={!!leftButton || !!rightButton}>
+        {children}
+      </ChildrenWrap>
+      {rightButton}
+    </Wrap>
+  );
+};
+
+export default Header;
+
+const Wrap = styled.div<{ isButton: boolean }>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: ${({ isButton }) => (isButton ? '14px 20px' : '16px 20px')};
+  ${({ isButton }) => !isButton && `justify-content: center;`}
+`;
+
+const ChildrenWrap = styled.div<{ isButton: boolean }>`
+  ${({ isButton }) =>
+    !!isButton &&
+    css`
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    `}
+`;
---
+++ src/components/Modal/style.ts
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const Title = styled.div`
+  font-size: 24px;
+`;
