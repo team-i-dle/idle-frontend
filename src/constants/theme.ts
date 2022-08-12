b043739665c23ca09099954b1d44808e7c4b421e
kiyoesjh <hyeyaa125@gmail.com>
2022-08-12 15:44:22 +0900

    Add: 스타일 관련 theme 추가

---
+++ src/constants/theme.ts
@@ -0,0 +1,80 @@
+export const colors = {
+  primary: '#4DF2B7',
+  primary400: '#00e08a',
+  primary300: '#00ea9d',
+  primary100: '#a4f8d4',
+  primary50: '#DAFDEE',
+  dim01: '#3EE4A9',
+
+  secondary: '#18191D',
+  gray01: '#27272A',
+  gray02: '#38393E',
+  gray03: '#56575C',
+  gray04: '#6a6b70',
+  gray05: '#929398',
+  gray06: '#b2b3b8',
+  gray07: '#d6d7dd',
+  gray08: '#e6e7ed',
+  gray09: '#f0f1f7',
+  gray10: '#f7f8fe',
+
+  error:  '#FB3E49',
+  warring:  '#FB993E',
+  info: '#00A9DE',
+
+  black: '#000000',
+  white: '#FFFFFF'
+};
+
+export const fonts = {
+  rg12: {
+    fontSize: '12px',
+    lineHeight: '14.4px',
+    fontWeight: 400
+  },
+  md12: {
+    fontSize: '12px',
+    lineHeight: '14.4px',
+    fontWeight: 500
+  },
+  rg14: {
+    fontSize: '14px',
+    lineHeight: '16.8px',
+    fontWeight: 400
+  },
+  md14: {
+    fontSize: '14px',
+    lineHeight: '16.8px',
+    fontWeight: 500
+  },
+  md16: {
+    fontSize: '16px',
+    lineHeight: '19.2px',
+    fontWeight: 500
+  },
+  sBold18: {
+    fontSize: '18px',
+    lineHeight: '21.6px',
+    fontWeight: 600
+  },
+  sBold20: {
+    fontSize: '20px',
+    lineHeight: '24px',
+    fontWeight: 600
+  },
+  sBold24: {
+    fontSize: '24px',
+    lineHeight: '28.8px',
+    fontWeight: 600
+  },
+  sBold28: {
+    fontSize: '28px',
+    lineHeight: '33.61px',
+    fontWeight: 600
+  },
+  sBold32: {
+    fontSize: '32px',
+    lineHeight: '38.41px',
+    fontWeight: 600
+  }
+}
