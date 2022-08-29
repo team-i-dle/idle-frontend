ef7406a584acefe4af3429fa64e6c570a697131e
kiyoesjh <hyeyaa125@gmail.com>
2022-08-30 08:54:02 +0900

    Add: Textarea 공통컴포넌트 추가

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,7 +1,9 @@
 import Text from "./text";
 import Radio from './radio';
+import Textarea from "./textarea";
 
 export default {
   Text,
-  Radio
+  Radio,
+  Textarea
 };
---
+++ src/components/Input/textarea/index.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+import defaultInputStyle from '../Input.style';
+
+type TextareaProps = JSX.IntrinsicElements['textarea'];
+
+type CustomTextareaProps = TextareaProps & {
+  resize?: string;
+};
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, CustomTextareaProps>(
+  (props, ref) => {
+    return <TextareaTag {...props} ref={ref} />;
+  }
+);
+
+export default Textarea;
+
+const TextareaTag = styled.textarea<CustomTextareaProps>`
+  ${defaultInputStyle};
+  min-height: 100px;
+  resize: ${({ resize }) => resize || 'none'};
+`;
